66eac8de117fc5012ad9b5d4e6242cf58e9d2aa6
heidi.seo <heidiseo1@gmail.com>
2020-04-05 14:43:08 +0100

    add api endpoint for /hello/:id

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.13.1"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.6.3",
   "com.typesafe.akka" %% "akka-stream" % "2.6.3",
-  "com.typesafe.akka" %% "akka-http" % "10.1.11"
+  "com.typesafe.akka" %% "akka-http" % "10.1.11",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.11"
 )
--- src/main/scala/http/Server.scala
+++ src/main/scala/http/Server.scala
@@ -3,24 +3,36 @@ package http
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{Directive, Route}
+import akka.http.scaladsl.server.Route
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
 
-object Server extends App {
+object Server extends App with DefaultJsonProtocol {
 
   val host = "0.0.0.0"
   val port = 8080
 
   implicit val system: ActorSystem = ActorSystem("helloworld")
   implicit val executor: ExecutionContext = system.dispatcher
+  implicit val personFormat: RootJsonFormat[Person] = jsonFormat3(Person)
 
   def route: Route =
-    (path("hello") & get & getQueryParams) {
-      case Person(Some(name), Some(country)) => complete(s"Hello, $name! You are from $country")
-      case Person(Some(name), None) => complete(s"Hello, $name!")
-      case Person(None, Some(country)) => complete(s"Hello stranger, you are from $country")
-      case Person(None, None) => complete("hello")
+    (path("hello") & get & parameters("name".?, "country".?)) { (name, country) =>
+      (name, country) match {
+        case (Some(name), Some(country)) => complete(s"Hello, ${name.capitalize}! You are from ${country.capitalize}.")
+        case (Some(name), None) => complete(s"Hello, ${name.capitalize}!")
+        case (None, Some(country)) => complete(s"Hello stranger, you are from ${country.capitalize}.")
+        case (None, None) => complete("hello")
+      }
+    } ~ (path("hello" / Segment) & get) { id =>
+      val result = group.find(_.id == id.toInt)
+      result match {
+        case Some(person) => complete(person.toJson)
+        case None => complete("This ID doesn't exist")
+      }
     }
 
   val bindingFuture = Http().bindAndHandle(route, host, port)
@@ -31,19 +43,18 @@ object Server extends App {
     => println(s"error: ${error.getMessage}")
   }
 
-  private def getQueryParams: Directive[Tuple1[Person]] = {
-    parameters("name".?, "country".?) tmap (Person.apply _).tupled
-  }
+  case class Person(id: Int, name: String, country: String)
 
-  case class Person(name: Option[String], country: Option[String])
 
-  case class Group(people: List[Person])
-
-  //
-  //  val dan: Person = Person("Dan", 29)
-  //  val heidi: Person = Person("Heidi", 32)
-  //  val bokyung: Person = Person("Bokyung", 61)
-  //  val group: Group = Group(List(dan, heidi, bokyung))
+  val bokyung: Person = Person(1, "Bokyung aka the mommy", "South Korea")
+  val donggi: Person = Person(2, "Donggi aka the brooo", "South Korea")
+  val hakyoung: Person = Person(3, "Hakyoung aka the daddy", "South Korea")
+  val namsoon: Person = Person(4, "Namsoon aka the granny", "South Korea")
+  val anna: Person = Person(5, "Anna aka Dongmin", "South Korea")
+  val dan: Person = Person(6, "Dan aka GingerViking", "England")
+  val heidi: Person = Person(7, "Heidi aka heiz", "South Korea")
+  val joe: Person = Person(8, "Joe aka the fat boiiii", "England")
+  val group: List[Person] = List(dan, heidi, bokyung, donggi, hakyoung, namsoon, anna, joe)
 
 
 }
